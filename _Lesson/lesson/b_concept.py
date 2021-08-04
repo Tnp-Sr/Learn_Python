7de1503b836524f26d82c6e3303c49d645c5ef75
Poly29887 <tanapon1325@gmail.com>
2021-08-05 00:35:27 +0700

    b-ตัวแปร, b-function, math op.

---
+++ _Lesson/lesson/b_concept.py
@@ -0,0 +1,24 @@
+print("\n")
+
+
+# --variable types--
+x = input("x = ") # รับค่า x (dynamic typing)
+print(x) # แสดง x
+
+w=int(input('width= ')) # รับค่า int
+h=float(input("height= ")) # รับค่า float
+print(w*h)
+
+print(type(w)) # บอก type ตัวแปร w
+print(type(h))
+print(type(x))
+t="text"
+s="sentence"
+print(type(t))
+print(type(s))
+
+
+print("\n")
+
+#---------------------------------------------------------
+
--- _Lesson/lesson/intro.py
+++ _Lesson/lesson/intro.py
@@ -1,5 +1,4 @@
-print('\n')
-print('Hello World!')
+print('\nHello World!')
 p=22/7
 P=5*6
 print(p)
@@ -7,4 +6,4 @@ print(P)
 t="testText"
 w='Xylitol'
 print(w +" "+t)
-print("--END--")
+print("--END--\n") 
