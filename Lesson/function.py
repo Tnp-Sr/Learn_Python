7417fc61ae16b88ec229205da87769e6fec8aeca
Poly29887 <tanapon1325@gmail.com>
2021-08-05 21:39:55 +0700

    add lambda

--- Lesson/function.py
+++ Lesson/function.py
@@ -9,3 +9,10 @@ h=2
 l=4
 print(get_area(w,h,l))
 print(get_area(w=2,h=-2,l=2))
+
+# lambda ช่วยให้เขียน function 1 line
+
+clean_text = lambda text : text.strip().capitalize()
+first_name = '    LAmbDa'
+output_name = clean_text(first_name)
+print(output_name)
