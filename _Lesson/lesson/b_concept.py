aed9f03c742409601268d930b8d9ef1cbea54d21
Poly29887 <tanapon1325@gmail.com>
2021-08-05 11:26:22 +0700

    learn condition, loop ,function, module, GUI-b

--- _Lesson/lesson/b_concept.py
+++ _Lesson/lesson/b_concept.py
@@ -13,10 +13,16 @@ print(type(w)) # บอก type ตัวแปร w
 print(type(h))
 print(type(x))
 t="text"
-s="sentence"
+s=str(12)
+print(s+" "+t)
 print(type(t))
 print(type(s))
 
+p=True
+q=False
+a1=p and q #False
+a2=p or q #True
+a3=not p #False
 
 print("\n")
 
---
+++ _Lesson/lesson/condition.py
@@ -0,0 +1,8 @@
+score=60
+if score >=80:
+    print("A")
+    print('Good')
+elif score>=70:
+    print('B')
+else :
+    print('Doon\'t Give Up')
