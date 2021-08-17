97c6e0dcc72b707274b07afa5dba731809a0299c
Poly29887 <tanapon1325@gmail.com>
2021-08-18 01:09:02 +0700

    modify 2d_list.py

--- Lesson/Basic concept/2d_list.py
+++ Lesson/Basic concept/2d_list.py
@@ -5,49 +5,59 @@ column 0  1  2
 row 0 [1  2  3] 
     1 [4  5  6]
 '''
+# print(a[0][1])
+# print(a[0])
+# b = a[1]
+# print(b)
 
-print(a[0][1])
-print(a[0])
-b = a[1]
-print(b)
-
-a[0][1] = 7
-
+# a[0][1] = 7
+# print(a[0][1])
+#
+# print(a)
 
 # แสดงผล
-'''
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        print(a[i][j], end=' ')
-    print()
-'''
-'''
-for row in a:
-    for elem in row:
-        print(elem, end=' ')
-    print()
-'''
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         print(a[i][j], end=' ')
+#     print()
+
+# for row in a:
+#     for elem in row:
+#         print(elem, end=' ')
+#     print()
+
+# for row in a:
+#     print(' '.join([str(elem) for elem in row]))
+
 
 # สร้าง
 m = 3 #column
 n = 3 #row
 b = [[0] * m for i in range(n)]  
-'''
-for row in b:
-    for elem in row:
-        print(elem, end=' ')
-    print()
-'''
+
+# for row in b:
+#     for elem in row:
+#         print(elem, end=' ')
+#     print()
+
 
 # รับค่า
-'''
-print()
 n = int(input())
 c = []
-# c = [[int(j) for j in input().split()] for i in range(n)]
 
-for row in a:
-    for elem in row:
-        print(elem, end=' ')
-    print()
+# for i in range(n):
+#     c.append([int(j) for j in input().split()])
+
+#  c = [[int(j) for j in input().split()] for i in range(n)]
+
+# for row in c:
+#     for elem in row:
+#         print(elem, end=' ')
+#     print()
+
+'''   
+3
+2 5 4
+2 6
+5 9 8 7
 '''
