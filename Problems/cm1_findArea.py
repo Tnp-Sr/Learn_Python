62d93459e0b889e7a8beba53af74f74783650a2b
Tnp_Sr <69105766+Tnp-Sr@users.noreply.github.com>
2022-07-22 18:26:17 +0700

    Create cm1_findArea.py

---
+++ Problems/cm1_findArea.py
@@ -0,0 +1,42 @@
+#Challenge by Me 1
+'''
+[Problem] ver 1
+P. have a menu to choose to find either area of triangle or rectangle.
+====Input==== 
+-selected num from the menu
+-width
+-height
+====Output====
+-area of selected geometry
+'''
+
+#function
+def rec(w,h):
+    return w * h
+
+def tri(w,h):
+    return 0.5 * w * h
+
+
+#main
+print("====Find Area====")
+print("select num")
+print("[1] rectangle")
+print("[2] triangle")
+print("=================")
+
+num = int(input("Choose number : "))
+w = int(input("\nInput width : "))
+h = int(input("Input height : "))
+
+if num > 0 and num <= 2 :
+    if num == 1 :
+        area = rec(w,h)
+    elif num == 2 :
+        area = tri(w,h)
+    print("area = ", area)
+else :
+    print("!! Wrong Input !!")
+
+
+
