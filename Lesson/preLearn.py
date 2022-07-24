cd9828ef71965b7b60b5ed8e07bf4d59143b868d
Tnp_Sr <69105766+Tnp-Sr@users.noreply.github.com>
2022-07-24 16:35:58 +0700

    Update preLearn.py
    delete short_if

--- Lesson/preLearn.py
+++ Lesson/preLearn.py
@@ -1,26 +1,14 @@
 ### Function
-# def rec(w,h):
-#     a=w*h
-#     return a
+def rec(w,h):
+    a=w*h
+    return a
 
 
-# w = int(input('w= '))
-# h = int(input('h= '))
-# print(rec(w,h))
+w = int(input('w= '))
+h = int(input('h= '))
+print(rec(w,h))
 
-### short if
-#full
-time = int(input('Time : '))
-if 12 <= time <= 24:
-    print("Afternoon")
-elif 0 <= time < 12 :
-    print("Morning")
-else:
-    print("Error!")
 
-#short
-print('...')
-print("Afternoon") if 12 <= time <= 24 else print("Morning") if 0 <= time < 12 else print("Error!")
 
 
 
