414fda060495c788ad927dd932b49853ef63b30a
Tnp_Sr <69105766+Tnp-Sr@users.noreply.github.com>
2022-07-24 16:36:07 +0700

    Create shortcut.py

---
+++ Lesson/Tricks/shortcut.py
@@ -0,0 +1,9 @@
+#### keyboardshortcut
+#alt+shift+up|down -> copy up|down
+#ctrl+[|] -> tab
+#alt+click -> multicursor
+#ctrl+alt+shift+up|down -> multicursor up|down
+#ctrl+shift+enter -> insert up
+#ctrl+enter -> insert down
+#ctrl+shift+l -> select all same words
+#ctrl++l -> select word
